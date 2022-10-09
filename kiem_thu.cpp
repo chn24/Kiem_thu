80e80835bff533b35d720a56e37451a022aa785a
chn24 <huynghia24082002@gmail.com>
2022-10-09 19:13:12 +0700

    btap tuan 5

---
+++ kiem_thu.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+string role;
+int num;
+
+int main () {
+    cin >> role >> num;
+    if(num < 0 || num > 5) {
+        cout << "Input không hợp lệ";
+    } else {
+        if(role == "Doctor") {
+        cout << "Không đặt được";
+        } else {
+            if (num >= 0 && num < 5) {
+                cout << " Đặt được";
+            } else if ( num == 5) {
+                cout << " Không đặt được";
+            }
+        }
+    }
+
+
+    return 0;
+}
