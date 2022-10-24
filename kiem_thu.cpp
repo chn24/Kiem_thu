aac2dde6631e4a6b492c7903ccca977cef371da3
chn24 <huynghia24082002@gmail.com>
2022-10-24 19:58:22 +0700

    update 24/10/2022

--- kiem_thu.cpp
+++ kiem_thu.cpp
@@ -4,21 +4,25 @@ using namespace std;
 string role;
 int num;
 
-int main () {
-    cin >> role >> num;
-    if(num < 0 || num > 5) {
-        cout << "Input không hợp lệ";
+void checkBooking (string role, int num) {//1
+    if(num < 0 || num > 5) {    //2
+        cout << "Input không hợp lệ";//3
     } else {
-        if(role == "Doctor") {
-        cout << "Không đặt được";
+        if(role == "Doctor") {//4
+        cout << "Không đặt được";//5
         } else {
-            if (num >= 0 && num < 5) {
-                cout << " Đặt được";
+            if (num >= 0 && num < 5) { //6
+                cout << " Đặt được"; //7
             } else {
-                cout << " Không đặt được";
+                cout << " Không đặt được"; //8
             }
         }
     }
+}
+
+int main () {
+    cin >> role >> num;
+    checkBooking(role, num);
 
 
     return 0;
